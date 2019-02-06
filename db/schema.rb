2140f3470a1aaad32012ab02f71d5800a45ecb74
vagrant <vagrant@vagrant.vm>
2019-02-06 00:49:41 +0000

    adição de usuarios e contatos, assim como seus seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_05_235739) do
+ActiveRecord::Schema.define(version: 2019_02_05_235938) do
+
+  create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.text "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_contacts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_02_05_235739) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contacts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+for i in 1..3
+	User.create({
+		email: Faker::Internet.free_email,
+		password: Faker::Internet.password(8)
+	}).contacts.create!(
+    [
+      {
+        name: Faker::Artist.name,
+        email: Faker::Internet.free_email,
+        phone: Faker::PhoneNumber.cell_phone,
+        description: Faker::Lorem.paragraph
+      },
+      {
+        name: Faker::Artist.name,
+        email: Faker::Internet.free_email,
+        phone: Faker::PhoneNumber.cell_phone,
+        description: Faker::Lorem.paragraph
+      },
+      {
+        name: Faker::Artist.name,
+        email: Faker::Internet.free_email,
+        phone: Faker::PhoneNumber.cell_phone,
+        description: Faker::Lorem.paragraph
+      },
+    ]
+  )
+end
